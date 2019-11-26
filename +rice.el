47c2fffe9aeeb1029325f6eea57428514e1bb2af
sleepyeye <wonjunlee.0729@gmail.com>
2019-11-26 17:49:37 +0900

    Add font rice and +rice.el script

---
+++ +rice.el
@@ -0,0 +1,11 @@
+;;; ~/.doom.d/+rice.el -*- lexical-binding: t; -*-
+
+;; TODO rice for org mode
+;; TODO rice for latex mode
+
+
+
+(def-package! mixed-pitch)
+
+
+(provide '+rice)
--- config.el
+++ config.el
@@ -6,7 +6,6 @@
 (load! "+lsp")
 (load! "+korean")
 
-;; (load! "+reveal")
 
 (defvar sleepyeye/dropbox-dir
   (expand-file-name "~/Dropbox"))
@@ -26,6 +25,28 @@
 (defvar sleepyeye/bibpdfs-dir
   (expand-file-name "pdf" sleepyeye/research-dir))
 
+;; Variable pitch font
+(defvar sleepyeye/font-write
+  (font-spec :family "Source Serif Pro"))
+
+;; Fixed pitch mono font
+(defvar sleepyeye/font-code
+  (font-spec :family "Source Code Pro" :size 18))
+
+(setq doom-font sleepyeye/font-code
+      doom-variable-pitch-font sleepyeye/font-write
+      ;; doom-serif-font  TODO this need fixed-pitch serif font
+      )
+
+(load-theme 'doom-solarized-light t)
+
+
+;; (set-face-font 'default "Source Code Pro Semibold-9")
+;; (set-face-font 'variable-pitch "Segoe UI Semibold-9")
+;; (copy-face 'default 'fixed-pitch)
+
+
+
 ;;(def-package! org-git-link)
 
 (def-package! ebib
@@ -35,3 +56,14 @@
         ebib-file-search-dirs (list sleepyeye/bibpdfs-dir)
         ebib-notes-directory sleepyeye/bibnote-dir
         ebib-preload-bib-files (list "master.bib")))
+
+(def-package! org-reverse-datetree)
+
+;; (def-package! org-starter
+;;   :config
+;;   (setq org-starter-path '(sleepyeye/research-dir sleepyeye/org-dir)))
+
+
+;; (org-starter-define-file "weekly.org"
+;;   :agenda t
+;;   :refile '(:maxlevel . 9))
