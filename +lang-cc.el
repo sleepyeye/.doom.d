1efe7bb6ee9ad72ed6566e88c8df3cd57225beaa
sleepyeye <wonjunlee.0729@gmail.com>
2020-01-31 22:57:03 +0900

    set cc-mode indentation style as LLVM

--- +lang-cc.el
+++ +lang-cc.el
@@ -5,7 +5,6 @@
 ;; TODO proper .clang-format
 ;; TODO fix weird filename.o file when I open cuda file
 
-
 (defun llvm-lineup-statement (langelem)
   (let ((in-assign (c-lineup-assignments langelem)))
     (if (not in-assign)
@@ -16,7 +15,7 @@
       in-assign)))
 
 ;; Add a cc-mode style for editing LLVM C and C++ code
-(c-add-style "llvm"
+(c-add-style "llvm.org"
              '("gnu"
                (fill-column . 80)
                (c++-indent-level . 2)
@@ -27,21 +26,14 @@
                                    (member-init-intro . ++)
                                    (statement-cont . llvm-lineup-statement)))))
 
-;; Files with "llvm" in their names will automatically be set to the
-;; llvm.org coding style.
-;; (add-hook 'c-mode-common-hook
-;;           (function
-;;            (lambda nil
-;;              (if (string-match "llvm" buffer-file-name)
-;;                  (progn
-;;                    (c-set-style "llvm.org"))))))
+;; (c-set-style "llvm.org")
 
 
 (after! cc-mode
   (setq-default c-basic-offset 2)
   (setq-default c-basic-offset tab-width
                 c-backspace-function #'delete-backward-char
-                c-default-style "llvm"))
+                c-default-style "llvm.org"))
 
 ;; (after! cuda-mode
 ;;   (add-hook! ))
